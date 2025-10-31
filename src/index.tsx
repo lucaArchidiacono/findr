85b3b8948dc90f3cf415b90a1857aa26648d056e
lucaArchidiacono <mail@archiluc.ch>
2025-11-01 00:09:30 +0100

    when debug showcase console

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,18 @@
 import { render } from "@opentui/react";
 import App from "./components/App";
+import { ConsolePosition } from "@opentui/core";
 
-render(<App />);
+render(<App />, {
+  ...(Bun.env.DEBUG === "true"
+    ? {
+        consoleOptions: {
+          position: ConsolePosition.LEFT,
+          sizePercent: 30,
+          colorInfo: "#00FFFF",
+          colorWarn: "#FFFF00",
+          colorError: "#FF0000",
+          startInDebugMode: false,
+        },
+      }
+    : undefined),
+});
